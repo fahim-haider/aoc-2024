3aa6e5c9a74978a1c2d00c9073b836fd91e383a7
fahim-haider <fahimhaider2@outlook.com>
2024-12-29 13:49:13 -0500

    Finish input file parsing

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,24 +1,16 @@
-
-// str is primitive, immutable
-// String is mutable, and its size is not known at compile time
+use std::fs;
 
 fn parse_input_file(input_file: &str) -> (Vec<i32>, Vec<i32>){
     let mut list1: Vec<i32> = Vec::new();
     let mut list2: Vec<i32> = vec![];
 
-    for line in input_file.lines() {
+    let contents = fs::read_to_string(input_file);
+
+    for line in contents.unwrap().lines() {
         let split_text = line.split(' ');
 
-        let number1 = split_text.clone().next().unwrap();
-        let number2 = split_text.last().unwrap();
-
-        println!("Number1 is {number1}");
-
-        //list1.push(number1.parse::<i32>().unwrap());
-        //list2.push(number2.parse::<i32>().unwrap());
-
-        //list1.push(split_text.clone().next().unwrap().parse::<i32>().unwrap());
-        //list2.push(split_text.last().unwrap().parse::<i32>().unwrap());
+        list1.push(split_text.clone().next().unwrap().parse::<i32>().unwrap());
+        list2.push(split_text.last().unwrap().parse::<i32>().unwrap());
     }
 
     return (list1, list2);
@@ -30,7 +22,14 @@ fn main() {
     
     let (mut vec1,mut vec2) = parse_input_file("src/input.txt");
 
-    //println!("With text:\nvec1: {} \t vec2: {}", vec1[0],vec2[0]);
+    println!("vec1[0]: {} \t vec2[0]: {}", vec1[0],vec2[0]);
 
     // 2. Calculate the difference between the arrays
 }
+
+/* Some notes!
+    - str is primitive, immutable
+    - String is mutable, and its size is not known at compile time
+    - unwrap returns the actual value from a result or option type
+    - split text returns an interator interestingly
+*/
