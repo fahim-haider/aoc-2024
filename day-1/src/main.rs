9cddc4f072af059ad8aa4e9b0565eaea942b1a66
fahim-haider <fahimhaider2@outlook.com>
2024-12-29 20:33:30 -0500

    Complete Day 1git add .

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn parse_input_file(input_file: &str) -> (Vec<i32>, Vec<i32>){
     let mut list1: Vec<i32> = Vec::new();
@@ -32,21 +33,51 @@ fn calculate_total_difference(vec1: &mut Vec<i32>, vec2: &mut Vec<i32>) -> (Vec<
     return (result, sum);
 }
 
-fn main() {
-    // Read the input file and fill in arrays
-    
-    let (mut vec1,mut vec2) = parse_input_file("src/input.txt");
+fn element_freq(vec2: &mut Vec<i32>) -> HashMap<i32, i32> {
+    let mut hash: HashMap<i32, i32> = HashMap::new();
 
+    for key in vec2.iter() {
+        match hash.get_mut(key) {
+            None => {
+                hash.insert(*key, 1);
+            }
+            Some(v) => {
+                *v += 1;
+            }
+        }
+    }  
+
+    return hash;
+}
+
+fn calc_similarity_score(vec1: &mut Vec<i32>, hash: HashMap<i32, i32>) -> i32 {
+    // Hash is a HashMap of the frequencies of Vector2! So we need to compare
+    let mut score: i32 = 0;
+    for value in vec1.iter(){
+        let frequency = hash.get(value).copied().unwrap_or(0);
+        score += *value * frequency;
+    }
+    return score;
+}
+
+fn main() {
+    // Part 1:
+    // Read the input file and fill in arrays
+
+    let (mut vec1,mut vec2) = parse_input_file("src/input.txt");
     println!("After parsing:\nvec1[0]: {} \t vec2[0]: {}", vec1[0],vec2[0]);
 
     // Sort arrays and calculate the difference between them in a new array
-
-    let (mut result, mut sum) = calculate_total_difference(&mut vec1, &mut vec2);
-
+    let (result, sum) = calculate_total_difference(&mut vec1, &mut vec2);
     println!("After calculations:\nvec1[0]: {}\t vec2[0]: {}\t result[0]: {}\t sum: {}", 
                 vec1[0],vec2[0],result[0], sum);
 
+    // Part 2:
+    // Count how many times a number in vec1 appears in vec2... use a hash map
+    let freq_vec2: HashMap<i32, i32> = element_freq(&mut vec2);
 
+    let similarity_score = calc_similarity_score(&mut vec1, freq_vec2);
+    println!("After similarity calculations, cumulative score: {similarity_score}");
 }
 
 /* Some notes!
