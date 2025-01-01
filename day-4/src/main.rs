44c0d58486ba3a9966849c3404ba2e57b433244c
fahim-haider <fahimhaider2@outlook.com>
2025-01-01 15:57:52 -0500

    learning alot and alot lol

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,54 +1,75 @@
+use itertools::Itertools;
 use std::fs;
-use grid::Grid;
+use ndarray::Array2;
 
-fn parse_input_file(input_file: &str) -> Grid<char> {
+const DIRECTIONS: &[(i32,i32)] = &[
+    // Straight
+    (0,-1),
+    (0,1),
+    (1,0),
+    (-1,0),
+
+    // Diagonal
+    (-1,-1),
+    (-1,1),
+    (1,-1),
+    (1,1)
+];
+
+fn parse_input_file(input_file: &str) -> Array2<char> {
     let contents = fs::read_to_string(input_file).unwrap();
 
-    Grid::parse(contents)
-    /*contents.lines()
-            .map(|st| st.chars().collect())
-            .collect::<Vec<_>>()*/
+    let vec = contents.lines()
+                        .map(|st| st.chars().collect::<Vec<_>>())
+                        .collect::<Vec<_>>();
+
+    let row_size = vec.len();
+    let col_size = vec[0].len();
+
+    let flat_vec = vec.into_iter().flatten().collect::<Vec<_>>();
+    Array2::<char>::from_shape_vec((row_size, col_size), flat_vec).unwrap()
 }
 
-fn search_matrix (rows: usize, columns: usize, matrix: &Vec<Vec<char>>) -> i32 {
+fn find_steps(point: &(i32,i32), num_steps: usize) -> Vec<(i32,i32)> {
+    let mut vec: Vec<(i32,i32)> = vec![(0,0)];
+    for i in 1..num_steps {
+        let (row,col) = *point;
+        let step = (row * i as i32, col * i as i32);
+        vec.push(step);
+    }
+    vec
+}
+
+fn search_matrix (matrix: &Array2<char>, pattern: &str) -> i32 {
+    // Find possible directions we should go
+    let directions = DIRECTIONS.iter()
+                        .map(|d| find_steps(d, pattern.len()))
+                        .collect::<Vec<_>>();
+
     let mut count: i32 = 0;
-    for row in 0..rows {
-        for column in 0..columns {
-            if matrix[row][column] == 'X' {
-                search_diagonally(row, column, matrix, &mut count);
-                search_vertically(row, column, matrix, &mut count);
-                search_horizontally(row, column, matrix, &mut count);
-            }
-        }
+    let (row_size, col_size) = (matrix.shape()[0],matrix.shape()[1]);
+    for (row, col) in (0..row_size).cartesian_product(0..col_size) {
+        // Find valid positions for this element
+        let valid_steps = directions.iter()
+                                .map(|d| 
+                                        d.iter()
+                                        .map(|(r, c)| (row as i32 + r, col as i32 + c))) 
+                                .filter(|d| 
+                                    d.clone().all(|(r, c)| r >= 0 && r < row_size as i32
+                                        && c >= 0 && c < col_size as i32))
+                                .map(|d| d.collect::<Vec<_>>())
+                                .collect::<Vec<_>>();
+
+        valid_steps.windows(4).map(f)
     }
     count
 }
 
-fn search_diagonally(row: usize, column: usize, matrix: &Vec<Vec<char>>, count: &mut i32) {
-    if row <= 2 && column <= 2 {
-        
-    } else if column <= 2 {
-        if (matrix[row][column], matrix[row+1][column+1], matrix[row+2][column+2], matrix[row+3][column+3]) == ('X','M','A','S') {
-            *count += 1;
-        }
-    } else if row >= matrix.len() - 2 {
-
-    } else if column >= matrix.len() - 2 {
-
-    } else {
-
-    }
-}
-
-fn search_vertically(row: usize, column: usize, matrix: &Vec<Vec<char>>, count: &mut i32) {
-
-}
-
-fn search_horizontally(row: usize, column: usize, matrix: &Vec<Vec<char>>, count: &mut i32) {
-
-}
-
 fn main() {
+    // Parse input file into a 2D array/matrix
     let content = parse_input_file("src/input.in");
-    println!("{}", content[0][5]);
+    println!("{}", content[[0,5]]);
+
+    // Part 1: Find all instances of XMAS patterns
+    search_matrix(&content, "XMAS");
 }
