1f186e2fbe16b3fc362da7e2981e6adf6ae3d22c
fahim-haider <fahimhaider2@outlook.com>
2025-01-03 11:36:55 -0500

    Finsh part 1 of day06!

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -4,29 +4,60 @@ use std::time::Instant;
 // Guard moves upwards initially
 const MOVE_UPWARDS: (i32, i32) = (-1, 0);
 
-const MAP_SIZE: i32 = 130;
+const MAP_SIZE: usize = 130;
 
 // Turn the guard right by swapping and negating vector elements
-fn turn_right(prev_r: i32, prev_c: i32) -> (i32, i32) {
-    (prev_c*-1, prev_r * -1)
+fn turn_right(direction: (i32,i32)) -> (i32, i32) {
+    (direction.1, direction.0 * -1)
 }
 
-fn create_map (file: &str) -> [[char; 130]; 130] {
+fn create_map (file: &str) -> ([[char; MAP_SIZE]; MAP_SIZE], (i32,i32)) {
     let contents = fs::read_to_string(file).unwrap();
 
-    let mut map = [['.'; 130]; 130];
+    let mut map = [['.'; MAP_SIZE]; MAP_SIZE];
+    let mut start: (i32, i32)= (0,0);
 
-    for (r, line) in contents.split('\n').enumerate() {
+    for (r, line) in contents.split_whitespace().enumerate() {
         for (c, ch) in line.chars().enumerate() {
             map[r][c] = ch;
+            if ch == '^' {
+                start = (r as i32,c as i32);
+            }
         }
     }
-    map
+    (map, start)
+}
+
+fn count_guard_positions (map: &mut [[char; MAP_SIZE]; MAP_SIZE], start: (i32,i32)) -> usize {
+    let mut count: usize = 0;
+    let mut point = start;
+    let mut direction = MOVE_UPWARDS;
+    // Keep going until we leave the map!
+    while point.0 + direction.0 < map.len() as i32 && point.0 + direction.0 >= 0 
+        && point.1 + direction.1 < map.len() as i32 && point.1 + direction.1 >= 0 {
+            // If obstacle infront of guard, turn right
+            if map[(point.0+direction.0) as usize]
+            [(point.1+direction.1) as usize] == '#' {
+                direction = turn_right(direction);
+            }
+            if map[(point.0+direction.0) as usize]
+            [(point.1+direction.1) as usize] != 'X' {
+                count += 1;
+            }
+            map[(point.0) as usize][(point.1) as usize] = 'X';
+            point.0 += direction.0;
+            point.1 += direction.1;
+            map[(point.0) as usize][(point.1) as usize] = '^';
+    }
+    count += 1; // For the last position in the array before it steps out
+    count
 }
 
 fn main() {
-    let start = Instant::now();
-    let map = create_map("src/input.in");
-    let duration = start.elapsed();
-    println!("Time taken = {:?}", duration)
+    let start_time = Instant::now();
+    let (mut map, start_point) = create_map("src/input.in");
+    let count = count_guard_positions(&mut map, start_point);
+    let duration = start_time.elapsed();
+    println!("Part 1: {count}");
+    println!("Time taken for Part 1 = {:?}", duration)
 }
