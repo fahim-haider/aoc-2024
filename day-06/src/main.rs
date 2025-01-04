6e30cbd7365a97afc405e7c5569d8509d5597eff
fahim-haider <fahimhaider2@outlook.com>
2025-01-04 11:21:18 -0500

    Fix lots of bugs in day06

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,10 +1,11 @@
+use std::collections::HashSet;
 use std::fs;
 use std::time::Instant;
 
 // Guard moves upwards initially
 const MOVE_UPWARDS: (i32, i32) = (-1, 0);
 
-const MAP_SIZE: usize = 10;
+const MAP_SIZE: usize = 130;
 
 #[derive(Clone, Debug)]
 struct Guard {
@@ -34,8 +35,11 @@ fn create_map (file: &str) -> ([[char; MAP_SIZE]; MAP_SIZE], Guard) {
     (map, guard)
 }
 
-fn count_guard_positions (map: &mut [[char; MAP_SIZE]; MAP_SIZE], guard: &mut Guard) -> usize {
-    let mut count: usize = 0;
+fn count_guard_positions (map: &mut [[char; MAP_SIZE]; MAP_SIZE], 
+                    guard: &mut Guard) -> (bool,HashSet<(i32,i32)>) {
+    let mut turn_points: Vec<((i32,i32),(i32,i32))> = vec![];
+    let mut step_pos: HashSet<(i32,i32)> = HashSet::new();
+    let mut is_loop: bool = false;
 
     // Keep going until we leave the map!
     while guard.pos.0 + guard.dir.0 < map.len() as i32 
@@ -44,31 +48,57 @@ fn count_guard_positions (map: &mut [[char; MAP_SIZE]; MAP_SIZE], guard: &mut Gu
         && guard.pos.1 + guard.dir.1 >= 0 {
             let next_cell = map[(guard.pos.0+guard.dir.0) as usize]
                                 [(guard.pos.1+guard.dir.1) as usize];
+            //println!("next_cell {:?}", next_cell);
             // If obstacle infront of guard, turn right
             if next_cell == '#' {
                 guard.dir = turn_right(guard.dir);
+                if !turn_points.contains(&(guard.pos,guard.dir)) {
+                    turn_points.push((guard.pos,guard.dir));
+                } else {
+                    //println!("Loop detected at {:?}", guard.pos);
+                    is_loop = true;
+                    return (is_loop,step_pos);
+                }
+            } else {
+                map[(guard.pos.0) as usize][(guard.pos.1) as usize] = 'X';
+                guard.pos.0 += guard.dir.0;
+                guard.pos.1 += guard.dir.1;
+                step_pos.insert(guard.pos);
+                map[(guard.pos.0) as usize][(guard.pos.1) as usize] = '^';
+                //println!("{:?}", guard.pos);
             }
-            // If next cell has not been traversed, count++
-            if next_cell != 'X' {
-                count += 1;
-            }
-            map[(guard.pos.0) as usize][(guard.pos.1) as usize] = 'X';
-            guard.pos.0 += guard.dir.0;
-            guard.pos.1 += guard.dir.1;
-            map[(guard.pos.0) as usize][(guard.pos.1) as usize] = '^';
     }
     // For the last position in the array before the guard steps out
     map[(guard.pos.0) as usize][(guard.pos.1) as usize] = 'X';
-    count += 1;
+    (is_loop,step_pos)
+}
+
+fn time_to_brute_force (base_map: &mut [[char; MAP_SIZE]; MAP_SIZE], 
+                base_guard: &mut Guard, step_pos: &mut HashSet<(i32,i32)>) -> usize {
+    let mut count:usize = 0;
+    count = step_pos.iter().filter(|a| {
+                            let mut map = base_map.clone();
+                            map[a.0 as usize][a.1 as usize] = '#';
+                            let mut guard = base_guard.clone();
+
+                            let res = count_guard_positions(&mut map, &mut guard);
+                            //println!("result of {:?}: {:?}, {:?}", a, res.0,res.1.len());
+                            res.0
+                }).count();
+
     count
 }
 
 fn main() {
+    let (map, guard) = create_map("src/input.in");
     let start_time = Instant::now();
-    let (mut map, mut start_point) = create_map("src/input.in");
-    let count = count_guard_positions(&mut map, &mut start_point);
+    let (_is_loop,mut step_pos) = count_guard_positions(&mut map.clone(), &mut guard.clone());
     let duration = start_time.elapsed();
-    println!("Part 1: {count}");
+    println!("Part 1: {:?}, {:?}", _is_loop, step_pos.len());
     println!("Time taken for Part 1 = {:?}", duration);
-    println!("The last row of the grid: {:?}", map[9]);
+    let start_time = Instant::now();
+    let count = time_to_brute_force(&mut map.clone(), &mut guard.clone(),&mut step_pos);
+    let duration = start_time.elapsed();
+    println!("Part 2: {:?}", count);
+    println!("Time taken for Part 2 = {:?}", duration);
 }
