14a5f6058e3c9be2ed51b6c04c41e41713e9b209
fahim-haider <fahimhaider2@outlook.com>
2025-01-04 13:12:33 -0500

    Tried optimizations to not much avail for day06

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -36,11 +36,14 @@ fn create_map (file: &str) -> ([[char; MAP_SIZE]; MAP_SIZE], Guard) {
 }
 
 fn count_guard_positions (map: &mut [[char; MAP_SIZE]; MAP_SIZE], 
-                    guard: &mut Guard) -> (bool,HashSet<(i32,i32)>) {
+                    guard: &mut Guard, obs_coord: (i32,i32)) -> (bool,HashSet<(i32,i32)>) {
     let mut turn_points: Vec<((i32,i32),(i32,i32))> = vec![];
     let mut step_pos: HashSet<(i32,i32)> = HashSet::new();
     let mut is_loop: bool = false;
-
+    step_pos.insert(guard.pos);
+    if obs_coord.0 >= 0 {
+        map[obs_coord.0 as usize][obs_coord.1 as usize] = '#';
+    }
     // Keep going until we leave the map!
     while guard.pos.0 + guard.dir.0 < map.len() as i32 
         && guard.pos.0 + guard.dir.0 >= 0 
@@ -57,47 +60,48 @@ fn count_guard_positions (map: &mut [[char; MAP_SIZE]; MAP_SIZE],
                 } else {
                     //println!("Loop detected at {:?}", guard.pos);
                     is_loop = true;
+                    if obs_coord.0 >= 0 {
+                        map[obs_coord.0 as usize][obs_coord.1 as usize] = '.';
+                    }
                     return (is_loop,step_pos);
                 }
             } else {
-                map[(guard.pos.0) as usize][(guard.pos.1) as usize] = 'X';
                 guard.pos.0 += guard.dir.0;
                 guard.pos.1 += guard.dir.1;
                 step_pos.insert(guard.pos);
-                map[(guard.pos.0) as usize][(guard.pos.1) as usize] = '^';
-                //println!("{:?}", guard.pos);
             }
     }
-    // For the last position in the array before the guard steps out
-    map[(guard.pos.0) as usize][(guard.pos.1) as usize] = 'X';
+    if obs_coord.0 >= 0 {
+        map[obs_coord.0 as usize][obs_coord.1 as usize] = '.';
+    }
     (is_loop,step_pos)
 }
 
-fn time_to_brute_force (base_map: &mut [[char; MAP_SIZE]; MAP_SIZE], 
+fn time_to_brute_force (mut base_map: &mut [[char; MAP_SIZE]; MAP_SIZE], 
                 base_guard: &mut Guard, step_pos: &mut HashSet<(i32,i32)>) -> usize {
-    let mut count:usize = 0;
-    count = step_pos.iter().filter(|a| {
-                            let mut map = base_map.clone();
-                            map[a.0 as usize][a.1 as usize] = '#';
+    step_pos.iter().filter(|a| {
+                            if **a == base_guard.pos {
+                                return false;
+                            }
                             let mut guard = base_guard.clone();
-
-                            let res = count_guard_positions(&mut map, &mut guard);
+                            let res = 
+                                count_guard_positions(&mut base_map, &mut guard, **a);
                             //println!("result of {:?}: {:?}, {:?}", a, res.0,res.1.len());
                             res.0
-                }).count();
-
-    count
+                }).count()
 }
 
 fn main() {
-    let (map, guard) = create_map("src/input.in");
+    let (mut map, guard) = create_map("src/input.in");
     let start_time = Instant::now();
-    let (_is_loop,mut step_pos) = count_guard_positions(&mut map.clone(), &mut guard.clone());
+    let (_is_loop,mut step_pos) = 
+        count_guard_positions(&mut map, &mut guard.clone(), (-1,-1));
     let duration = start_time.elapsed();
     println!("Part 1: {:?}, {:?}", _is_loop, step_pos.len());
     println!("Time taken for Part 1 = {:?}", duration);
     let start_time = Instant::now();
-    let count = time_to_brute_force(&mut map.clone(), &mut guard.clone(),&mut step_pos);
+    let count = 
+        time_to_brute_force(&mut map, &mut guard.clone(),&mut step_pos);
     let duration = start_time.elapsed();
     println!("Part 2: {:?}", count);
     println!("Time taken for Part 2 = {:?}", duration);
