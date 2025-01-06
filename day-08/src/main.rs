1e7c3e9fe8942856a9704a05f5073f185e319189
fahim-haider <fahimhaider2@outlook.com>
2025-01-05 21:10:32 -0500

    Complete day08

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -10,11 +10,7 @@ struct AntennaPoint {
     r: i32,
     c: i32,
 }
-
 impl AntennaPoint {
-    fn get_coord_usize (&self) -> (usize,usize) {
-        (self.r as usize, self.c as usize)
-    }
     fn get_anti_nodes (&mut self, p2: &mut AntennaPoint) -> (AntiNode,AntiNode) {
         let displacement = (p2.r - self.r, p2.c - self.c);
         
@@ -34,7 +30,6 @@ struct AntiNode {
     c: i32,
     is_valid: bool
 }
-
 impl AntiNode {
     // Logic to check if antinode is valid
     fn new(r: i32, c: i32) -> AntiNode {
@@ -45,39 +40,37 @@ impl AntiNode {
         }
         AntiNode {r, c, is_valid}
     }
-    fn get_coord_usize (&self) -> (usize,usize) {
-        (self.r as usize, self.c as usize)
-    }
 }
 
-fn get_resonant_anti_nodes (p1: &mut AntennaPoint, p2: &mut AntennaPoint, set: &mut HashSet<AntiNode>) {
+fn get_resonant_anti_nodes (p1: &AntennaPoint, p2: &AntennaPoint, set: &mut HashSet<AntiNode>) {
     let distance = (p2.r - p1.r, p2.c - p1.c);
-    // displacement in one direction
+    // holds displacement in one direction... will be incremented in a loop
     let mut displacement1 = (p1.r + 2*distance.0, p1.c + 2*distance.1);
-    // displacement in another direction
+    // holds displacement in the other direction... will be incremented in a loop
     let mut displacement2 = (p1.r - distance.0, p1.c - distance.1);
-    while displacement1.0 <= MAP_SIZE as i32 && displacement1.1 <= MAP_SIZE as i32
-    && displacement1.0 > 0 as i32 && displacement1.1 > 0 as i32  {
-        let antinode1 = AntiNode::new(p1.r + 2*distance.0, 
-            p1.c + 2*distance.1);
+    // Increment displacement1 in steps of the distance between points
+    while displacement1.0 < MAP_SIZE as i32 && displacement1.1 < MAP_SIZE as i32
+    && displacement1.0 >= 0 as i32 && displacement1.1 >= 0 as i32  {
+        let antinode1 = AntiNode::new(displacement1.0, 
+            displacement1.1);
         set.insert(antinode1);
-        displacement1.0 += 2*distance.0;
-        displacement1.1 += 2*distance.1;
+        displacement1.0 += distance.0;
+        displacement1.1 += distance.1;
     }
-    while displacement2.0 <= MAP_SIZE as i32 && displacement2.1 <= MAP_SIZE as i32
-    && displacement2.0 > 0 as i32 && displacement2.1 > 0 as i32  {
-        let antinode1 = AntiNode::new(p1.r + 2*distance.0, 
-            p1.c + 2*distance.1);
+    // Increments displacement2 similarly
+    while displacement2.0 < MAP_SIZE as i32 && displacement2.1 < MAP_SIZE as i32
+    && displacement2.0 >= 0 as i32 && displacement2.1 >= 0 as i32  {
+        let antinode1 = AntiNode::new(displacement2.0, 
+            displacement2.1);
         set.insert(antinode1);
-        displacement2.0 += 2*distance.0;
-        displacement2.1 += 2*distance.1;
+        displacement2.0 -= distance.0;
+        displacement2.1 -= distance.1;
     }
 }
 
 // Create array of map and store all locations of each antenna type
 fn create_map (file: &str) -> ([[char; MAP_SIZE]; MAP_SIZE], HashMap<char, Vec<AntennaPoint>>) {
     let contents = fs::read_to_string(file).unwrap();
-
     let mut map = [['.'; MAP_SIZE]; MAP_SIZE];
     let mut hash: HashMap<char, Vec<AntennaPoint>> = HashMap::new();
 
@@ -100,18 +93,40 @@ fn create_map (file: &str) -> ([[char; MAP_SIZE]; MAP_SIZE], HashMap<char, Vec<A
     (map, hash)
 }
 
+// Finds all antinodes based on critera from part 1 (only two antinodes per pair of antennas)
 fn find_antinodes (mut hash: HashMap<char, Vec<AntennaPoint>>) -> usize {
     let mut set: HashSet<AntiNode> = HashSet::new();
-
     for vec in hash.values_mut() {
         for i in 0..vec.len() {
             for j in i..(vec.len()-1) {
                 let (left,right) = vec.split_at_mut(i+1);
                 let curr_point = &mut left[i];
                 let next_point = &mut right[j-i];
-                //println!("Current points to try: curr_ {:?}, next_ {:?}", curr_point,next_point);
-                let mut ans: HashSet<AntiNode> = HashSet::new();
-                get_resonant_anti_nodes(curr_point, next_point, &mut ans);
+                let (an1, an2) = curr_point.get_anti_nodes(next_point);
+                if an1.is_valid == true {
+                    set.insert(an1);
+                }
+                if an2.is_valid == true {
+                    set.insert(an2);
+                }
+            }
+        }
+    }
+    set.len()
+}
+
+// Finds all antinodes including the antennas themselves AND resonant harmonics
+fn find_resonant_antinodes (mut hash: HashMap<char, Vec<AntennaPoint>>) -> usize {
+    let mut set: HashSet<AntiNode> = HashSet::new();
+    for vec in hash.values_mut() {
+        for i in 0..vec.len() {
+            for j in i..(vec.len()-1) {
+                let (left,right) = vec.split_at_mut(i+1);
+                let curr_point = &mut left[i];
+                let next_point = &mut right[j-i];
+                set.insert(AntiNode {r: curr_point.r, c: curr_point.c, is_valid: true});
+                set.insert(AntiNode {r: next_point.r, c: next_point.c, is_valid: true});
+                get_resonant_anti_nodes(curr_point, next_point, &mut set);
             }
         }
     }
@@ -120,15 +135,20 @@ fn find_antinodes (mut hash: HashMap<char, Vec<AntennaPoint>>) -> usize {
 
 fn main() {
     let start_time = Instant::now();
-    let (map, hash) = create_map("src/input.in");
-    let sum = find_antinodes(hash);
+    let (_map, hash) = create_map("src/input.in");
+    let sum = find_antinodes(hash.clone());
     let duration = start_time.elapsed();
     println!("Part 1: {:?}", sum);
     println!("Time taken for Part 1 = {:?}", duration);
+    let start_time = Instant::now();
+    let sum = find_resonant_antinodes(hash);
+    let duration = start_time.elapsed();
+    println!("Part 2: {:?}", sum);
+    println!("Time taken for Part 2 = {:?}", duration);
 }
 /*
 
 RESULTS:
-Part 1:  1.12 ms!!!
-Part 2: 
+Part 1: 265, solved in 1.12 ms!!!
+Part 2: 962, solved in 1.49 ms!
 */
